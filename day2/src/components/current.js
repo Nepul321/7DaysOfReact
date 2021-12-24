9f3cacfb1ce13edf72eb6ea1db5744120b9efcd2
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-24 14:20:13 -0500

    Finish project for day 2

--- day2/src/components/current.js
+++ day2/src/components/current.js
@@ -1,7 +1,31 @@
-function Current() {
+function Current(props) {
+    const {item} = props
+    const ingredients = item.ingredients
     return (
         <div className="currentCoffee my-3">
-        <h1>Current</h1>
+            <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src={item.image} class="img-fluid rounded-start" alt="" />
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{item.name}</h5>
+                    <p class="card-text">{item.description}</p>
+                    <p class="card-text">
+                         {
+                         ingredients.map((ingredient) => {
+                           return (
+                               <p>{ingredient}</p>
+                           )
+                         })
+
+                         }
+                    </p>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
--- day2/src/components/home.js
+++ day2/src/components/home.js
@@ -1,46 +1,62 @@
 import Current from "./current";
-import {useState, useEffect} from 'react'
-
+import { useState, useEffect } from "react";
 
 function Home() {
-    const [Items, setItems] = useState([]);
-    const [currentItem, setCurrent] = useState({});
+  const [Items, setItems] = useState([]);
+  const [currentItem, setCurrent] = useState({});
 
-    useEffect(() => {
-        const xhr = new XMLHttpRequest();
-        const method = "GET";
-        const url = 'http://localhost:8000/api/coffee/'
-        const responseType = "json"
-        xhr.responseType = responseType
-        xhr.open(method, url)
-        xhr.onload = () => {
-           const serverResponse = xhr.response;
-           var listedItems = serverResponse;
-           if (xhr.status === 200) {
-               setItems(listedItems);
-           } else {
-               alert("An error occured")
-           }
-        }
-       
-        xhr.send();
-    }, [])
-    console.log(Items)
-    return (
-        <div className="home">
-            <Current />
-            <div className="coffeelist my-3">
-                {Items.map((item) => {
-                    return (
-                        <div className="image mb-3">
-                            <img src={item.image} alt="" style={{width : "260px"}}/>
-                        </div>
-                        
-                        )
-                })}
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    const method = "GET";
+    const url = "http://localhost:8000/api/coffee/";
+    const responseType = "json";
+    xhr.responseType = responseType;
+    xhr.open(method, url);
+    xhr.onload = () => {
+      const serverResponse = xhr.response;
+      var listedItems = serverResponse;
+      if (xhr.status === 200) {
+        setItems(listedItems);
+        setCurrent(listedItems[0]);
+      } else {
+        alert("An error occured");
+      }
+    };
+
+    xhr.send();
+  }, []);
+
+  function HandleImageClick(id) {
+    const filtered = Items.filter((item) => {
+      return item.id === id;
+    });
+
+    const obj = filtered[0];
+
+    setCurrent(obj);
+  }
+  return (
+    <div className="home">
+      <Current item={currentItem} />
+      <div className="coffeelist my-3">
+        {Items.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="image mb-3"
+              onClick={() => HandleImageClick(item.id)}
+            >
+              <img
+                src={item.image}
+                alt=""
+                style={{ objectFit: "cover", width: "260px", cursor : "pointer"}}
+              />
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
